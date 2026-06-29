35b65c56647512e47c822fffb242aa6222c2343d
Pioneer-Stomic <pioneerstomic@163.com>
2026-06-29 11:10:22 +0800

    C++ learning

---
+++ C++/itheima/code/047_pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int num{ 10 };
+
+    int *p;
+    p = &num;
+
+    std::cout << "num = " << num << '\n';
+    std::cout << "num的地址为 " << &num << '\n'; 
+    std::cout << "指针p为 " << p << '\n';
+
+    std::cout << "指针p占用 " << sizeof(p) << " 个字节\n";
+
+    *p = 20;
+
+    std::cout << "num = " << num << '\n';
+    std::cout << "*p = " << *p << '\n';
+
+    return 0;
+}
