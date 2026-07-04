33d3316f1ed5102054da01acb4767afbd2c961b7
Pioneer-Stomic <pioneerstomic@163.com>
2026-07-04 20:46:18 +0800

    C++ learning: 指针

--- C++/itheima/code/06_Pointers/052_pass_by_pointer.cpp
+++ C++/itheima/code/06_Pointers/052_pass_by_pointer.cpp
@@ -11,48 +11,43 @@ int main()
     int* p1{ &num1 };
     int* p2{ &num2 };
 
-    std::cout << "main: " << p1 << " " << p2 << '\n';
+    std::cout << "交换前main中num1, num2的地址: " << p1 << " " << p2 << '\n';
+    std::cout << "交换前main中p1, p2的地址: " << &p1 << " " << &p2 << '\n' << '\n';
 
     swap01(&num1, &num2);
-    std::cout << "main: " << num1 << " " << num2 << '\n';
+    std::cout << "使用swap01交换后main中num1, num2的值: " << num1 << " " << num2 << '\n';
+    std::cout << "使用swap01交换后main中num1, num2的地址: " << p1 << ' ' << p2 << '\n' << '\n';
 
     num1 = 10;
     num2 = 20;
 
     swap02(&num1, &num2);
-    std::cout << "main: " << num1 << " " << num2 << '\n';
+    std::cout << "使用swap02交换后main中num1, num2的值: " << num1 << " " << num2 << '\n';
+    std::cout << "使用swap02交换后main中num1, num2的地址: " << p1 << ' ' << p2 << '\n' << '\n';    
 
     return 0;
 }
 
 // 地址传递：交换指针指向的值，改变实参和形参
 void swap01(int* p1, int* p2)
-{
-    int** q1{ &p1 };
-    int** q2{ &p2 };
-   
+{  
     int temp{ *p1 };
     *p1 = *p2;
     *p2 = temp;
 
-    std::cout << "swap01: " << p1 << " " << p2 << '\n';
-    std::cout << "pswap01: " << q1 << " " << q2 << '\n';      
-
-    std::cout << "nswap01: " << *p1 << " " << *p2 << '\n';
+    std::cout << "swap01中num1, num2的地址: " << p1 << " " << p2 << '\n';
+    std::cout << "swap01中p1, p2的地址: " << &p1 << " " << &p2 << '\n';      
+    std::cout << "swap01中num1, num2的值: " << *p1 << " " << *p2 << '\n';
 }
 
 // 交换内存：交换形参指针的指向，仅改变形参
 void swap02(int* p1, int* p2)
 {
-    int** q1{ &p1 };
-    int** q2{ &p2 };
-    
-    int *temp{ p1 };
+    int* temp{ p1 };
     p1 = p2;
     p2 = temp;
 
-    std::cout << "swap02: " << p1 << " " << p2 << '\n';  
-    std::cout << "pswap02: " << q1 << " " << q2 << '\n';  
-
-    std::cout << "nswap02: " << *p1 << " " << *p2 << '\n';
+    std::cout << "swap02中num1, num2的地址: " << p1 << " " << p2 << '\n';  
+    std::cout << "swap02中p1, p2的地址: " << &p1 << " " << &p2 << '\n';  
+    std::cout << "swap02中num1, num2的值: " << *p1 << " " << *p2 << '\n';
 }
